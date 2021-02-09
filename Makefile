6d6ca8566127559e14b2959fad9941c102203905
Fredrik-Westergard <fredrik.westergard@ha.ax>
2021-02-09 14:51:01 +0200

    added some more test cases in linkedTest.c and updated Makefile

--- Makefile
+++ Makefile
@@ -2,16 +2,14 @@ CFLAGS = -Wall -pedantic
 
 all: corona-App linkedTest dateTest clean
 
-
 corona-App: main.o utilities.o date.o linkedList.o
 	gcc $(CFLAGS) main.o utilities.o date.o linkedList.o -o corona -lm
 
-
 dateTest: date.o
-	gcc dateTest.c date.o $(CFLAGS) -o dateTests
+	gcc $(CFLAGS) dateTest.c date.o -o dateTests
 
-linkedTest: linkedList.o date.o
-	gcc linkedTest.c linkedList.o date.o $(CFLAGS) -o linkedListTest
+linkedTest: linkedList.o date.o utilities.o
+	gcc $(CFLAGS) linkedTest.c linkedList.o date.o utilities.o -o linkedListTest -lm
 
 main.o: utilities.h date.h linkedList.h
 	gcc $(CFLAGS) -c main.c -o main.o
--- linkedTest.c
+++ linkedTest.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "date.h"
 #include "linkedList.h"
+#include "utilities.h"
 
 int main(int argc, char const *argv[])
 {    
@@ -41,13 +42,52 @@ int main(int argc, char const *argv[])
     d6.month = 3;
     d6.year = 1994;
     addToList(l, 666666, d6);
+
+    date d7;
+    d7.day = 9;
+    d7.month = 2;
+    d7.year = 2021;
+    addToList(l, 123456, d7);
+
+    date d8;
+    d8.day = 18;
+    d8.month = 1;
+    d8.year = 2021;
+    addToList(l, 222222, d8);
+
+    date d9;
+    d9.day = 19;
+    d9.month = 1;
+    d9.year = 2021;
+    addToList(l, 333333, d9);
+
+    date d10;
+    d10.day = 4;
+    d10.month = 2;
+    d10.year = 2021;
+    addToList(l, 444444, d10);
+
+    date d11;
+    d11.day = 12;
+    d11.month = 12;
+    d11.year = 1992;
+    addToList(l, 555555, d11);
+
+    date d12;
+    d12.day = 13;
+    d12.month = 3;
+    d12.year = 1994;
+    addToList(l, 666666, d12);
     
     printList(l);
 
-    removeFromList(l, 0);
+    removeTooOld(l);
+
+    printf("\n");
+    printList(l);
 
     removeFromList(l, 4);
-
+    removeFromList(l, 0);
     removeFromList(l, 2);
 
 
